5c6aef1cbc28ade2270864767f269fa2b9121d74
nabakolu <lnabakowski@mailo.com>
2020-10-15 23:51:13 +0200

    Made preview of media with mediainfo use less space. Replace less with cat to get "binary" output of non text files

--- .config/preview.sh
+++ .config/preview.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 case "$1" in
-	*.mp3|*.mp4|*.wav|*.opus|*.ogg|*.mkv) mediainfo "$1";;
+	*.mp3|*.mp4|*.wav|*.opus|*.ogg|*.mkv) 
+		str=$(mediainfo "$1")
+		echo "${str//                          /}";;
 	*.png|*.jpg|*.jpeg) chafa "$1";;
 	*.zip) zipinfo -2 -h "$1";;
 	*.gz) tar -ztf "$1";;
 	*.bz2) tar -jtf "$1";;
 	*.tar) tar -tf "$1";;
 	*.pdf) pdftotext "$1" -;;
-	*) less "$1";;
+	*) cat "$1";;
 esac
