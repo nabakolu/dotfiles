568c503fbddc49115a4f290e36ba02fb48b89fc3
internal-arch <lnabakowski@mailo.com>
2021-01-15 21:34:58 +0100

    Qutebrowser: redirect twitter.com to nitter.unixfox.eu

--- .config/qutebrowser/redirects.py
+++ .config/qutebrowser/redirects.py
@@ -6,5 +6,8 @@ def rewrite(request: qutebrowser.api.interceptor.Request):
     if url == 'reddit.com' or url == 'www.reddit.com':
         request.request_url.setHost('teddit.net')
         request.redirect(request.request_url)
+    if url == 'twitter.com' or url == 'www.twitter.com':
+        request.request_url.setHost('nitter.unixfox.eu')
+        request.redirect(request.request_url)
 
 qutebrowser.api.interceptor.register(rewrite)
