91f4efff4e050e27d635e2d392012d967aa59805
nabakolu <lnabakowski@mailo.com>
2022-02-03 13:13:10 +0100

    Remade qutebrowser redirects to make old.reddit redirect work

--- .config/qutebrowser/redirects.py
+++ .config/qutebrowser/redirects.py
@@ -1,13 +1,28 @@
-import qutebrowser.api.interceptor
+import operator, re, typing
+from urllib.parse import urljoin
 
-def rewrite(request: qutebrowser.api.interceptor.Request):
-    url = request.request_url.host()
+from qutebrowser.api import interceptor, message
+from PyQt5.QtCore import QUrl
 
-    if url == 'reddit.com' or url == 'www.reddit.com':
-        request.request_url.setHost('teddit.net')
-        request.redirect(request.request_url)
-    if url == 'twitter.com' or url == 'www.twitter.com':
-        request.request_url.setHost('nitter.unixfox.eu')
-        request.redirect(request.request_url)
+# Any return value other than a literal 'False' means we redirected
+REDIRECT_MAP = {
+	"reddit.com": operator.methodcaller('setHost', 'old.reddit.com'),
+	"www.reddit.com": operator.methodcaller('setHost', 'old.reddit.com'),
+    "twitter.com": operator.methodcaller('setHost', 'nitter.unixfox.eu'),
+	"www.twitter.com": operator.methodcaller('setHost', 'nitter.unixfox.eu'),
 
-qutebrowser.api.interceptor.register(rewrite)
+} # type: typing.Dict[str, typing.Callable[..., typing.Optional[bool]]]
+
+def int_fn(info: interceptor.Request):
+	"""Block the given request if necessary."""
+	if (info.resource_type != interceptor.ResourceType.main_frame or
+			info.request_url.scheme() in {"data", "blob"}):
+		return
+	url = info.request_url
+	redir = REDIRECT_MAP.get(url.host())
+	if redir is not None and redir(url) is not False:
+		message.info("Redirecting to " + url.toString())
+		info.redirect(url)
+
+
+interceptor.register(int_fn)
