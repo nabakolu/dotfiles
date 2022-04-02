2674b6ba5caf75ca608b576bf4f02a23ce437528
nabakolu <lnabakowski@mailo.com>
2022-04-02 14:25:36 +0200

    Cleaned status bar code a bit

--- .config/update_bar.sh
+++ .config/update_bar.sh
@@ -1,52 +1,25 @@
+/bin/ls /media/* && usbicon="  禍"
 
-#volstat=$(amixer get Master | grep %)
-#volstat=$(amixer -c 2 get Speaker | grep % | grep Left)
-pulse=true
-
-if [ "$(lsblk | grep -E '(sdc|sdd)')" != "" ]
-#if [ "$(lsblk | grep -E '(sda|sdc|sdd)')" != "" ] # depending on amount if internal drives, adjust the amount of sdb/sdc/sdd/...
-then
-    usbicon=" 禍"
+vol=$(pamixer --get-volume)
+if [ "$vol" -gt "70" ]; then
+	volicon="墳"
+elif [ "$vol" -lt "30" ]; then
+	volicon="奄"
+else
+	volicon="奔"
 fi
-
-
-if [ $pulse ]
+if [ "$(pamixer --list-sinks | grep bluez)" != "" ]
+then
+	volicon=""
+fi
+if [ "$(pamixer --get-mute)" = "true" ]
 	then
-	vol=$(pamixer --get-volume)
-	if [ "$vol" -gt "70" ]; then
-		volicon="墳"
-	elif [ "$vol" -lt "30" ]; then
-		volicon="奄"
-	else
-		volicon="奔"
-	fi
+	volicon="婢"
 	if [ "$(pamixer --list-sinks | grep bluez)" != "" ]
 	then
-		volicon=""
+		volicon=""
 	fi
-	if [ "$(pamixer --get-mute)" = "true" ]
-		then
-		volicon="婢"
-		if [ "$(pamixer --list-sinks | grep bluez)" != "" ]
-		then
-			volicon=""
-		fi
-	fi
-
-elif [ "$(echo "$volstat" | grep off)" = "" ]
-		then
-    	vol=$(echo "$volstat" | grep -o "\[[0-9]\+%\]" | sed 's/[^0-9]*//g')
-		if [ "$vol" -gt "70" ]; then
-			volicon="墳"
-		elif [ "$vol" -lt "30" ]; then
-			volicon="奄"
-		else
-			volicon="奔"
-		fi
-else
-    	volicon="婢"
 fi
-
 case $(xset q | grep LED | tail -c 2) in
 	"0") lock="";;
 	"1") lock=" וּ ";;
@@ -54,12 +27,7 @@ case $(xset q | grep LED | tail -c 2) in
 	"3") lock=" וּ   ";;
 esac
 
-if [ "$(cat /sys/class/net/wlp8s0/operstate)" = "down" ]
-then
-    wifiicon="睊"
-else
-    wifiicon="直"
-fi
+[ "$(cat /sys/class/net/wlp8s0/operstate)" = "down" ] && wifiicon="睊" || wifiicon="直"
 
 
 /bin/ls /sys/class/power_supply/BAT0 > /dev/null && battery="$(cat /sys/class/power_supply/BAT0/capacity)" && batteryicon=" "
