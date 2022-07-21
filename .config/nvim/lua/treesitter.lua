5cf4b7d8c01fa41be1d8fcd6a43dbbf17112b67d
nabakolu <lnabakowski@mailo.com>
2022-07-21 14:45:48 +0200

    Made treesitter use a different auto installer, since it broke after update

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -1,8 +1,19 @@
+local parsers = require'nvim-treesitter.parsers'
+function _G.ensure_treesitter_language_installed()
+  local lang = parsers.get_buf_lang()
+  if parsers.get_parser_configs()[lang] and not parsers.has_parser(lang) then
+    vim.schedule_wrap(function()
+    vim.cmd("TSInstallSync "..lang)
+    vim.cmd[[e!]]
+    end)()
+  end
+end
+
+vim.cmd[[autocmd FileType * :lua ensure_treesitter_language_installed()]]
+
+
 local configs = require("nvim-treesitter.configs")
 configs.setup {
-	ensure_installed = "maintained",
-	sync_install = false, 
-	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "" }, -- list of language that will be disabled
