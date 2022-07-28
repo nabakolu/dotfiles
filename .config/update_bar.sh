b4603e51c50adc258a159176d671b9167c1c620c
internal-arch <lnabakowski@mailo.com>
2022-07-29 01:36:56 +0200

    update_bar.sh now uses escape sequence for colors in dwm

--- .config/update_bar.sh
+++ .config/update_bar.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 /bin/ls /media/* && usbicon="  禍"
 
 vol=$(pamixer --get-volume)
@@ -10,7 +11,7 @@ else
 fi
 if [ "$(pamixer --list-sinks | grep bluez)" != "" ]
 then
-	volicon=""
+	volicon=""
 fi
 if [ "$(pamixer --get-mute)" = "true" ]
 	then
@@ -27,9 +28,11 @@ case $(xset q | grep LED | tail -c 2) in
 	"3") lock=" וּ   ";;
 esac
 
-[ "$(cat /sys/class/net/wlp8s0/operstate)" = "down" ] && wifiicon="睊" || wifiicon="直"
+[ "$(networkctl status 2> /dev/null | grep Address)" = "" ] && wifiicon="睊" || wifiicon="直"
 
-
+date=" $(date +'%a %d %b %Y')"
+time=" $(date +'%T')"
 /bin/ls /sys/class/power_supply/BAT0 > /dev/null && battery="$(cat /sys/class/power_supply/BAT0/capacity)" && batteryicon=" "
 
-xsetroot -name "          $lock$batteryicon$battery$usbicon  $volicon$vol%  $wifiicon   $(date +'%a %d %b %Y')   $(date +'%T')"
+status="$lock$batteryicon$battery$usbicon  $volicon$vol%  $wifiicon  $date  $time"
+xsetroot -name "    $status"
