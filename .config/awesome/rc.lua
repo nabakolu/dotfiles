a0e0ce22926ffe7b2673d77eedf3fdb97bc67c8b
nabakolu <lnabakowski@mailo.com>
2022-07-31 00:27:40 +0200

    Made League of Legends work by excluding it from the default awesome rule

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -472,7 +472,7 @@ root.keys(globalkeys)
 -- Rules to apply to new clients (through the "manage" signal).
 awful.rules.rules = {
     -- All clients will match this rule.
-    { rule = {},
+    { rule = { except = { instance = "League of Legends (TM) Client" },},
         properties = { border_width = beautiful.border_width,
             border_color = beautiful.border_normal,
             focus = awful.client.focus.filter,
@@ -481,7 +481,6 @@ awful.rules.rules = {
             buttons = clientbuttons,
             screen = awful.screen.preferred,
             placement = awful.placement.no_overlap + awful.placement.no_offscreen,
-            size_hints_honot = true,
         }
     },
     { rule_any = { class = { "qutebrowser" } }, properties = { tag = root.tags()[1] } },
