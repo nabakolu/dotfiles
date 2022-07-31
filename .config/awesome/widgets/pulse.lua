6b34612d461a7784775b172dd85cf5a1b72113b9
nabakolu <lnabakowski@mailo.com>
2022-07-31 18:22:38 +0200

    Created own pulse widget for awesome

---
+++ .config/awesome/widgets/pulse.lua
@@ -0,0 +1,85 @@
+local awful = require "awful"
+local gears = require "gears"
+local wibox = require "wibox"
+local beautiful = require "beautiful"
+
+-- Pulse
+local pulse = wibox.widget.textbox()
+local volume
+local icon
+
+local tooltip = awful.tooltip {
+    objects        = { pulse },
+    timer_function = function()
+        local script = [[ pactl get-default-sink ]]
+        awful.spawn.easy_async_with_shell(script, function(stdout)
+            res = tostring(stdout)
+        end)
+        return res
+    end,
+}
+
+local function update_volume()
+    local scriptVolume = [[
+    pamixer --get-volume
+    ]]
+    local handle = io.popen(scriptVolume)
+    volume = handle:read("*a")
+    handle:close()
+end
+
+local function update_icon()
+    local bt = false
+    local scriptMute = [[
+    pamixer --get-mute
+    ]]
+    local scriptSink = [[
+    pamixer --get-default-sink
+    ]]
+    local handle = io.popen(scriptSink)
+    if handle:read("*a"):find("bluez",1,true) then
+        bt = true
+    end
+    local handle = io.popen(scriptMute)
+    if handle:read("*a"):find("false",1,true) then
+        if bt then
+            icon = ""
+        else
+            icon = "墳"
+        end
+    else
+        if bt then
+            icon = ""
+        else
+            icon = "婢"
+        end
+    end
+    handle:close()
+end
+
+local function update_all()
+    update_volume()
+    update_icon()
+    pulse.markup = icon..volume
+end
+
+gears.timer {
+    timeout = 10,
+    autostart = true,
+    call_now = true,
+    callback = function() update_all() end
+}
+
+
+awesome.connect_signal('update-pulse-volume', function()
+    update_volume()
+    pulse.markup = icon..volume
+end)
+
+awesome.connect_signal('update-pulse-icon', function()
+    update_icon()
+    pulse.markup = icon..volume
+end)
+
+
+return pulse
