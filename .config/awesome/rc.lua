61feb0e194d66956822f6b46618188192076508f
internal-arch <lnabakowski@mailo.com>
2022-08-02 02:07:38 +0200

    Made awesome have rounded corners

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -608,6 +608,9 @@ client.connect_signal("manage", function(c)
 end)
 
 
+client.connect_signal("manage", function (c)
+    c.shape = gears.shape.rounded_rect
+end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
