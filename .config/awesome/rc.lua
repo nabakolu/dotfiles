d6225d0e8026a0649a8dcd9b80c3168f0f2b457c
nabakolu <lnabakowski@mailo.com>
2022-11-21 16:31:53 +0100

    Made awesomewm help use the Unicode chars I want

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -580,8 +580,6 @@ awful.rules.rules = {
 }
 -- }}}
 
--- {{{ Signals
--- Signal function to execute when a new client appears.
 client.connect_signal("manage", function(c)
     -- Set the windows at the slave,
     -- i.e. put it at the end of others instead of setting it master.
@@ -595,47 +593,6 @@ client.connect_signal("manage", function(c)
     end
 end)
 
--- Add a titlebar if titlebars_enabled is set to true in the rules.
-client.connect_signal("request::titlebars", function(c)
-    -- buttons for the titlebar
-    local buttons = gears.table.join(
-        awful.button({}, 1, function()
-            c:emit_signal("request::activate", "titlebar", { raise = true })
-            awful.mouse.client.move(c)
-        end),
-        awful.button({}, 3, function()
-            c:emit_signal("request::activate", "titlebar", { raise = true })
-            awful.mouse.client.resize(c)
-        end)
-    )
-
-    awful.titlebar(c):setup {
-        { -- Left
-            awful.titlebar.widget.iconwidget(c),
-            buttons = buttons,
-            layout  = wibox.layout.fixed.horizontal
-        },
-        { -- Middle
-            { -- Title
-                align  = "center",
-                widget = awful.titlebar.widget.titlewidget(c)
-            },
-            buttons = buttons,
-            layout  = wibox.layout.flex.horizontal
-        },
-        { -- Right
-            awful.titlebar.widget.floatingbutton(c),
-            awful.titlebar.widget.closebutton(c),
-            layout = wibox.layout.fixed.horizontal()
-        },
-        layout = wibox.layout.align.horizontal
-    }
-end)
-
--- Enable sloppy focus, so that focus follows mouse.
--- client.connect_signal("mouse::enter", function(c)
---     c:emit_signal("request::activate", "mouse_enter", {raise = false})
--- end)
 
 client.connect_signal("manage", function(c)
     if not awesome.startup then awful.client.setslave(c) end
@@ -659,27 +616,37 @@ screen.connect_signal("arrange", function (s)
     end
 end)
 
--- client.connect_signal("manage", function (c)
---     c.shape = gears.shape.rect
--- end)
--- 
--- client.connect_signal("property::fullscreen", function(c)
---     if c.fullscreen then
---         c.shape = gears.shape.rect
---     else
---     c.shape = gears.shape.rect
---     end
--- end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
--- }}}
 
 client.connect_signal("manage", function(c)
     c.shape = function(cr, w, h)
         gears.shape.rounded_rect(cr, w, h, 10)
     end
 end)
+
+
+hotkeys_popup.widget.labels = {
+    XF86AudioMute = "婢",
+    XF86AudioLowerVolume = "",
+    XF86AudioRaiseVolume = "墳",
+    Return = "Enter",
+    BackSpace = "Backspace",
+    Delete = "Delete",
+    XF86AudioPrev = "玲",
+    XF86AudioNext = "怜",
+    XF86AudioStop = "栗",
+    XF86AudioPlay = "契",
+    space = "Space",
+    Up = "",
+    Down = "",
+    Left = "",
+    Right = "",
+    Control = "Ctrl",
+    Mod4 = "",
+    Shift = "ﰵ",
+}
 
 -- set wallpaper
 awful.spawn.with_shell("~/.fehbg")
