bc45cd3e3783d65acc009a97156cadebed4c83b7
nabakolu <lnabakowski@mailo.com>
2023-02-23 18:12:32 +0100

    Add missing config files for neovim ui overhaul

---
+++ .config/nvim/lua/nabakolu/plugins/config/lualine.lua
@@ -0,0 +1,50 @@
+require('lualine').setup {
+    options = {
+        component_separators = '|',
+        section_separators = '',
+        globalstatus = true,
+        theme = 'pywal',
+    },
+    sections = {
+        lualine_b = {
+            {
+                'diagnostics',
+
+                -- Table of diagnostic sources, available sources are:
+                --   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
+                -- or a function that returns a table as such:
+                --   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
+                sources = { 'nvim_diagnostic' },
+
+                -- Displays diagnostics for the defined severity types
+                sections = { 'error', 'warn' },
+
+                diagnostics_color = {
+                    -- Same values as the general color option can be used here.
+                    error = 'DiagnosticLualineError', -- Changes diagnostics' error color.
+                    warn  = 'DiagnosticLualineWarn',  -- Changes diagnostics' warn color.
+                },
+                symbols = { error = " ", warn = " "},
+                colored = true,           -- Displays diagnostics status in color if set to true.
+                update_in_insert = false, -- Update diagnostics in insert mode.
+                always_visible = true,   -- Show diagnostics even if there are none.
+            }
+        },
+        lualine_x = {
+            {
+                'filetype',
+                colored = false,   -- Displays filetype icon in color if set to true
+                icon_only = false, -- Display only an icon for filetype
+                icon = { align = 'left' }, -- Display filetype icon on the right hand side
+                -- icon =    {'X', align='right'}
+                -- Icon string ^ in table is ignored in filetype component
+            }
+        },
+        lualine_y = {
+            {
+                'progress',
+                color = 'Normal',
+            }
+        },
+    }
+}
