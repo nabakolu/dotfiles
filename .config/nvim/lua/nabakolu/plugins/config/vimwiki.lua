fbaf47c345d952fc80da68ad059193df278b255b
nabakolu <lnabakowski@mailo.com>
2023-08-22 13:40:45 +0200

    Disable global vimwiki

--- .config/nvim/lua/nabakolu/plugins/config/vimwiki.lua
+++ .config/nvim/lua/nabakolu/plugins/config/vimwiki.lua
@@ -12,7 +12,7 @@ let wiki_1.ext = '.md'
 
 let g:vimwiki_list = [wiki_1]
 let g:vimwiki_ext2syntax = {'.md': 'markdown', '.markdown': 'markdown', '.mdown': 'markdown'}
-let g:vimwiki_global_ext = 1
+let g:vimwiki_global_ext = 0
 let g:vimwiki_markdown_link_ext = 1
 let g:vimwiki_automatic_nested_syntaxes = 1
 ]]
