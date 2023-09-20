7ef093b34dc8a4c14c214458e805f1bf43ad8333
nabakolu <lnabakowski@mailo.com>
2023-09-20 16:35:28 +0200

    Made awesomewm datetime widget show all events for the next 7 days

--- .config/awesome/widgets/datetime.lua
+++ .config/awesome/widgets/datetime.lua
@@ -10,9 +10,9 @@ tooltip:add_to_object(datetime)
 tooltip.mode = "inside"
 tooltip.gaps = 5
 datetime:connect_signal("mouse::enter", function()
-    local script = [[ khal list | ( ! grep ^ ) && echo "No Events Today" ]]
+    local script = [[ khal --color list -f "{start-time-full}-{end-time-full} {title}"  today $(date +%a) | sed -e 's/\x1b\[0m//' -e 's/\x1b\[0m/<\/b>/' -e 's/\x1b\[1m/<b>/' ]]
     awful.spawn.easy_async_with_shell(script, function(stdout)
-        tooltip.text = tostring(stdout)
+        tooltip.markup = stdout
     end)
 end)
 
