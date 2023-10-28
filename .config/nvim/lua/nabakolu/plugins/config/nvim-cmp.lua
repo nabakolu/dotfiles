a744ff5b406754d018c3426b8a9ac92f28666017
nabakolu <lnabakowski@mailo.com>
2023-10-28 15:14:15 +0200

    Remove unneeded example from nvim-cmp config

--- .config/nvim/lua/nabakolu/plugins/config/nvim-cmp.lua
+++ .config/nvim/lua/nabakolu/plugins/config/nvim-cmp.lua
@@ -74,11 +74,3 @@ cmp.setup.cmdline('/', {
         { name = 'buffer' }
     }
 })
-
--- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
--- The following example advertise capabilities to `clangd`.
-require 'lspconfig'.clangd.setup {
-    capabilities = capabilities,
-}
