c937799e5efe63ecec1db906f0a10d72e93252ae
nabakolu <lnabakowski@mailo.com>
2023-11-27 16:43:47 +0100

    Added spoofed keybinds for whatsdesk and one for discord

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -514,6 +514,62 @@ clientkeys = gears.table.join(
 
 )
 
+
+whatsdeskkeys = gears.table.join(
+    awful.key({ "Control" }, "j",
+        function (c)
+            -- awful.spawn.with_shell("sleep 0.1; xdotool key --clearmodifiers F")
+            root.fake_input("key_release", "j")
+            root.fake_input("key_release", "Control_L")
+            root.fake_input("key_press"  , "Return")
+            root.fake_input("key_release", "Return")
+            root.fake_input("key_press"  , "Control_L")
+        end
+    ),
+    awful.key({ "Control" }, "h",
+        function (c)
+            -- awful.spawn.with_shell("sleep 0.1; xdotool key --clearmodifiers F")
+            root.fake_input("key_release", "h")
+            root.fake_input("key_release", "Control_L")
+            root.fake_input("key_press"  , "BackSpace")
+            root.fake_input("key_release", "BackSpace")
+            root.fake_input("key_press"  , "Control_L")
+        end
+    ),
+    awful.key({ "Control" }, "n",
+        function (c)
+            -- awful.spawn.with_shell("sleep 0.1; xdotool key --clearmodifiers F")
+            root.fake_input("key_release", "n")
+            root.fake_input("key_release", "Control_L")
+            root.fake_input("key_press"  , "Down")
+            root.fake_input("key_release", "Down")
+            root.fake_input("key_press"  , "Control_L")
+        end
+    ),
+    awful.key({ "Control" }, "p",
+        function (c)
+            -- awful.spawn.with_shell("sleep 0.1; xdotool key --clearmodifiers F")
+            root.fake_input("key_release", "p")
+            root.fake_input("key_release", "Control_L")
+            root.fake_input("key_press"  , "Up")
+            root.fake_input("key_release", "Up")
+            root.fake_input("key_press"  , "Control_L")
+        end
+    ),
+    awful.key({ "Control" }, "k",
+        function (c)
+            -- awful.spawn.with_shell("sleep 0.1; xdotool key --clearmodifiers F")
+            root.fake_input("key_release", "k")
+            root.fake_input("key_release", "Control_L")
+            root.fake_input("key_press"  , "Alt_L")
+            root.fake_input("key_press"  , "k")
+            root.fake_input("key_release", "k")
+            root.fake_input("key_release", "Alt_L")
+            root.fake_input("key_press"  , "Control_L")
+        end
+    )
+)
+
 discordkeys = gears.table.join(
     awful.key({ "Control" }, "j",
         function (c)
@@ -522,6 +578,16 @@ discordkeys = gears.table.join(
             root.fake_input("key_release", "Control_L")
             root.fake_input("key_press"  , "Return")
             root.fake_input("key_release", "Return")
+            root.fake_input("key_press"  , "Control_L")
+        end
+    ),
+    awful.key({ "Control" }, "h",
+        function (c)
+            -- awful.spawn.with_shell("sleep 0.1; xdotool key --clearmodifiers F")
+            root.fake_input("key_release", "h")
+            root.fake_input("key_release", "Control_L")
+            root.fake_input("key_press"  , "BackSpace")
+            root.fake_input("key_release", "BackSpace")
             root.fake_input("key_press"  , "Control_L")
         end
     ),
@@ -659,7 +725,7 @@ awful.rules.rules = {
     },
     { rule_any = { class = { "qutebrowser" } }, properties = { tag = root.tags()[1] } },
     { rule_any = { class = { "discord" } }, properties = { keys = gears.table.join(clientkeys, discordkeys),  tag = root.tags()[2] } },
-    { rule_any = { class = { "whatsdesk" } }, properties = { tag = root.tags()[2] } },
+    { rule_any = { class = { "whatsdesk" } }, properties = { keys = gears.table.join(clientkeys, whatsdeskkeys),  tag = root.tags()[2] } },
     { rule_any = { name = { "Auto-Type - KeePassXC" } }, properties = { keys = gears.table.join(clientkeys, keepassxckeys) } },
 
     { rule_any = { class = { "FLOATING" } },
