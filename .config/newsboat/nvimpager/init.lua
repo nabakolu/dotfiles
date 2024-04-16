c14271240781fb2192e25c708905a9974c5b401f
nabakolu <lnabakowski@mailo.com>
2024-04-16 02:46:47 +0200

    Added image support to newsboat via pandoc, glow, nvimpager and image.nvim

---
+++ .config/newsboat/nvimpager/init.lua
@@ -0,0 +1,56 @@
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
+
+vim.cmd('colorscheme xresources')
+vim.keymap.set('n', 'h', ':quit<CR>')
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+    {
+        "3rd/image.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            {
+                "nvim-treesitter/nvim-treesitter",
+                build = ":TSUpdate",
+                config = function()
+                    require("nvim-treesitter.configs").setup({
+                        ensure_installed = { "markdown" },
+                        highlight = { enable = true },
+                    })
+                end,
+            },
+        },
+        opts = {
+            backend = "kitty",
+            integrations = {
+                markdown = {
+                    enabled = true,
+                    clear_in_insert_mode = false,
+                    download_remote_images = true,
+                    only_render_image_at_cursor = false,
+                    filetypes = { "man" }, -- markdown extensions (ie. quarto) can go here
+                }
+            },
+            max_width = nil,
+            max_height = nil,
+            max_width_window_percentage = nil,
+            max_height_window_percentage = nil,
+            kitty_method = "normal",
+        },
+    },
+}
+
+require("lazy").setup( plugins)
