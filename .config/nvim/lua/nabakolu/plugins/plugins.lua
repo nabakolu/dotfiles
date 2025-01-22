919f7e2fb90cca36a886d747f17e37faa6aee565
nabakolu <lnabakowski@mailo.com>
2025-01-22 12:45:40 +0100

    Add vim-dadbod to nvim

--- .config/nvim/lua/nabakolu/plugins/plugins.lua
+++ .config/nvim/lua/nabakolu/plugins/plugins.lua
@@ -136,6 +136,42 @@ local plugins = {
             kitty_method = "normal",
         },
     },
+    {
+        'kristijanhusak/vim-dadbod-ui',
+        dependencies = {
+            { 'tpope/vim-dadbod',                     lazy = true },
+            { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+        },
+        cmd = {
+            'DBUI',
+            'DBUIToggle',
+            'DBUIAddConnection',
+            'DBUIFindBuffer',
+        },
+        init = function()
+            -- Your DBUI configuration
+            vim.g.db_ui_use_nerd_fonts = 1
+            vim.g.db_ui_notification_width = 20
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "dbui",
+                callback = function()
+                    vim.api.nvim_buf_set_keymap(0, "n", "<C-j>", "<Plug>(DBUI_SelectLine)",
+                        { noremap = false, silent = true })
+                end,
+            })
+            -- automatically resize output to be half window height
+            vim.api.nvim_create_autocmd("BufEnter", {
+                pattern = "*",
+                callback = function()
+                    if vim.bo.filetype == "dbout" then
+                        local win_height = vim.api.nvim_get_option("lines")
+                        local half_height = math.floor(win_height * 0.5)
+                        vim.cmd("resize " .. half_height)
+                    end
+                end,
+            })
+        end,
+    }
 }
 
 require("lazy").setup(
