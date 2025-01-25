5db04b1460ccdc6df920cbe16fd812ee5d1e813f
nabakolu <lnabakowski@mailo.com>
2025-01-25 14:34:27 +0100

    Add old reddit redirect

---
+++ .config/qutebrowser/greasemonkey/old.reddit.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         Reddit Old Layout
+// @namespace    9bf36c681d35d52913b3bda74ddf6127318ed7b0
+// @version      0.2
+// @description  Forces usage of the old reddit version
+// @author       /u/AyrA_ch
+// @author       nabakolu
+// @match        https://www.reddit.com/*
+// @match        https://reddit.com/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // Exclude media URLs since they can't be accessed on old.reddit.com
+  if (
+    window.location.href.includes("reddit.com/media") ||
+    window.location.href.includes("preview.redd.it")
+  ) {
+    return; // Exit the script if the URL is a media URL
+  }
+
+  // Redirect to old.reddit.com
+  top.location.hostname = "old.reddit.com";
+})();
+
+/*
+LICENSE:
+This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+The full license text can be found here: http://creativecommons.org/licenses/by-nc-sa/4.0/
+The link has an easy to understand version of the license and the full license text.
+
+DISCLAIMER:
+THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
+*/
