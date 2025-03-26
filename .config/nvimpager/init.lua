60226315ffa9e04d950ebeb776da61fdb4385275
nabakolu <lnabakowski@mailo.com>
2025-03-26 15:46:26 +0100

    Make nvimpager always be in pager mode, so that colors are always right Make nvimpager hide its cursor

--- .config/nvimpager/init.lua
+++ .config/nvimpager/init.lua
@@ -1,2 +1,9 @@
-vim.opt.guicursor = 'n:hor50-blinkwait0-blinkoff25-blinkon25'
 vim.cmd('colorscheme xresources')
+
+-- hide Cursor
+vim.api.nvim_set_hl(0, 'Cursor', {
+  blend = 100,
+  fg = 'NONE',
+  bg = '#FFFFFF',
+})
+vim.opt.guicursor:append("a:Cursor/lCursor")
