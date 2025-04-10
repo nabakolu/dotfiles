f0f64041e8af6ce998651df48150eec3eccdfb6c
nabakolu <lnabakowski@mailo.com>
2025-04-10 13:14:22 +0200

    Set nvim diagnostic symbols with new not deprecated way

--- .config/nvim/lua/nabakolu/plugins/config/lspconfig.lua
+++ .config/nvim/lua/nabakolu/plugins/config/lspconfig.lua
@@ -92,14 +92,22 @@ vim.diagnostic.config {
     float = { border = "single" },
     underline = { severity = vim.diagnostic.severity.ERROR },
     virtual_text = false,
+    signs = { 
+        text = {
+            [vim.diagnostic.severity.ERROR] = '󰅚 ', 
+            [vim.diagnostic.severity.WARN] = ' ', 
+            [vim.diagnostic.severity.HINT] = '󰌶 ', 
+            [vim.diagnostic.severity.INFO] = ' ', 
+        }
+    }
 }
 
 require('lspconfig.ui.windows').default_options = {
     border = "single"
 }
 
-local signs = { Error = "󰅚 ", Warn = " ", Hint = "󰌶 ", Info = " " }
-for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+-- local signs = { Error = "󰅚 ", Warn = " ", Hint = "󰌶 ", Info = " " }
+-- for type, icon in pairs(signs) do
+--     local hl = "DiagnosticSign" .. type
+--     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+-- end
