732cd3532433edd2e24114c529bee111925ff05d
nabakolu <lnabakowski@mailo.com>
2025-05-06 18:59:40 +0200

    Make mpv show playlist until c-l pressed again Allow remove last file in playlist in mpv

--- .config/mpv/scripts/remove_current.lua
+++ .config/mpv/scripts/remove_current.lua
@@ -5,13 +5,8 @@ local function remove_current_file()
 
     -- Check if there is more than one file in the playlist
     if playlist_count > 1 then
-        -- Ensure the current file isn't the last one in the playlist
-        if current_index >= 0 and current_index < playlist_count - 1 then
-            mp.commandv("playlist-remove", current_index)
-            mp.osd_message("Removed current file from playlist")
-        else
-            mp.osd_message("Cannot remove the last file")
-        end
+        mp.commandv("playlist-remove", current_index)
+        mp.osd_message("Removed current file from playlist")
     else
         mp.osd_message("Cannot remove the only file in the playlist")
     end
