69f10a8700b6156f1b06b611f4c238e9a9f5624c
nabakolu <lnabakowski@mailo.com>
2025-05-09 12:20:34 +0200

    Fix telescope keybinds descriptions in nvim

--- .config/nvim/lua/nabakolu/plugins/telescope.lua
+++ .config/nvim/lua/nabakolu/plugins/telescope.lua
@@ -19,11 +19,11 @@ return {
       },
     }
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>jj', builtin.find_files, {}, { desc = "telescope find file" })
-    vim.keymap.set('n', '<leader>jg', builtin.live_grep, {}, { desc = "telescope live grep" })
-    vim.keymap.set('n', '<leader>jb', builtin.buffers, {}, { desc = "telescope buffers" })
-    vim.keymap.set('n', '<leader>jh', builtin.help_tags, {}, { desc = "telescope help tags" })
-    vim.keymap.set('n', '<leader>jr', builtin.lsp_references, {}, { desc = "telescope lsp references" })
-    vim.keymap.set('n', '<leader>jd', builtin.lsp_document_symbols, {}, { desc = "telescope lsp document symbols" })
+    vim.keymap.set('n', '<leader>jj', builtin.find_files, { desc = "telescope find file" })
+    vim.keymap.set('n', '<leader>jg', builtin.live_grep, { desc = "telescope live grep" })
+    vim.keymap.set('n', '<leader>jb', builtin.buffers, { desc = "telescope buffers" })
+    vim.keymap.set('n', '<leader>jh', builtin.help_tags, { desc = "telescope help tags" })
+    vim.keymap.set('n', '<leader>jr', builtin.lsp_references, { desc = "telescope lsp references" })
+    vim.keymap.set('n', '<leader>jd', builtin.lsp_document_symbols, { desc = "telescope lsp document symbols" })
   end
 }
