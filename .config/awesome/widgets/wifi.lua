caf51d05e74cd7d5089a6206c5c3aeb7d7ebeb06
nabakolu <lnabakowski@mailo.com>
2025-05-17 01:20:25 +0200

    Make all connections shown in awesomewm bar, with lan, wifi, vpn, and no connection having their own sybmols

--- .config/awesome/widgets/wifi.lua
+++ .config/awesome/widgets/wifi.lua
@@ -5,43 +5,51 @@ local beautiful = require "beautiful"
 
 -- Wifi
 local wifi = wibox.widget.textbox()
-local tooltip = awful.tooltip { }
+local tooltip = awful.tooltip {}
 tooltip:add_to_object(wifi)
 tooltip.mode = "inside"
 tooltip.gaps = 5
 
 wifi:connect_signal("mouse::enter", function()
-    local script = [[ nmcli con show --active | tail -n +2 ]]
-    awful.spawn.easy_async_with_shell(script, function(stdout)
-        tooltip.text = tostring(stdout:gsub("\n[^\n]*$", ""))
-    end)
+  local script = [[ nmcli con show --active | tail -n +2 ]]
+  awful.spawn.easy_async_with_shell(script, function(stdout)
+    tooltip.text = tostring(stdout:gsub("\n[^\n]*$", ""))
+  end)
 end)
 
 local function get_wifi()
-    local script = [[
-	nmcli g | tail -1 | awk '{printf $1}'
-	]]
+  local script = [[
+  nmcli connection show --active | awk -F'  +' 'NR>1 {print $3}'
+  ]]
 
-    awful.spawn.easy_async_with_shell(script, function(stdout)
-        local status = tostring(stdout)
-        if not status:match("disconnected") then
-            wifi.markup = " 󰖩 "
-        else
-            wifi.markup = " 󰖪 "
-        end
-    end)
+  awful.spawn.easy_async_with_shell(script, function(stdout)
+    local status = tostring(stdout)
+    wifi.markup = " "
+    for line in status:gmatch("[^\r\n]+") do
+      if line == "ethernet" then
+        wifi.markup = wifi.markup .. "󰌘 "
+      elseif line == "wifi" then
+        wifi.markup = wifi.markup .. "󰖩 "
+      elseif line == "vpn" then
+        wifi.markup = wifi.markup .. " "
+      end
+    end
+    if wifi.markup == " " then
+        wifi.markup = " 󰌙 "
+    end
+  end)
 end
 
 gears.timer {
-    timeout = 5,
-    autostart = true,
-    call_now = true,
-    callback = function() get_wifi() end
+  timeout = 5,
+  autostart = true,
+  call_now = true,
+  callback = function() get_wifi() end
 }
 
 wifi:buttons(gears.table.join(
-    awful.button({ }, 1, function () awful.spawn("networkmanager_dmenu"); end),
-    awful.button({ }, 3, function () awful.spawn("networkmanager_dmenu"); end)
-    ))
+  awful.button({}, 1, function() awful.spawn("networkmanager_dmenu"); end),
+  awful.button({}, 3, function() awful.spawn("networkmanager_dmenu"); end)
+))
 
 return wifi
