93b9db9bb169977598003d92b178054a7d54333f
nabakolu <lnabakowski@mailo.com>
2025-09-01 22:20:20 +0200

    Add Link list to end of article in newsboat pandoc filter

--- .config/newsboat/filter.lua
+++ .config/newsboat/filter.lua
@@ -1,32 +1,33 @@
 Link_COUNT = 0
+local links = {}
 
-local maxCharsPerLine = 60
-local function insertLineBreaks(inputString)
-    local lines = {}
-    local currentLine = ""
-    for word in inputString:gmatch("%S+") do
-        if #currentLine + #word + 1 > maxCharsPerLine then
-            table.insert(lines, currentLine)
-            currentLine = ""
-        end
-        if currentLine == "" then
-            currentLine = word
-        else
-            currentLine = currentLine .. " " .. word
-        end
-    end
-    if currentLine ~= "" then
-        table.insert(lines, currentLine)
-    end
-    return table.concat(lines, "\n")
+local function register_url(url)
+    Link_COUNT = Link_COUNT + 1
+    table.insert(links, url)
+    return Link_COUNT
 end
 
 function Image(elem)
-    Link_COUNT = Link_COUNT +1
-    return { "![" .. tostring(Link_COUNT) .. "](" .. elem.src ..")", "\n",  insertLineBreaks(tostring(elem.title)) }
+    local num = register_url(elem.src)
+    return { "![" .. tostring(num) .. "](" .. elem.src .. ")", "\n", tostring(elem.title) }
 end
 
 function Link(elem)
-    Link_COUNT = Link_COUNT +1
-    return pandoc.utils.stringify(elem.content) .. " [" .. tostring(Link_COUNT) .. "]"
+    local num = register_url(elem.target)
+    return pandoc.utils.stringify(elem.content) .. " [" .. tostring(num) .. "]"
+end
+
+function Pandoc(doc)
+    if #links > 0 then
+        local inlines = { pandoc.Str("Article Links:"), pandoc.LineBreak() }
+        for i, url in ipairs(links) do
+            table.insert(inlines, pandoc.Str("[" .. tostring(i) .. "] "))
+            table.insert(inlines, pandoc.Str(url))
+            if i < #links then
+                table.insert(inlines, pandoc.LineBreak())
+            end
+        end
+        table.insert(doc.blocks, pandoc.Para(inlines))
+    end
+    return doc
 end
