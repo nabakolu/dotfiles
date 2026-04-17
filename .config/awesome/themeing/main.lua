24f2fd3e14a603996fdcef4b6b43b7156c5ccae2
nabakolu <lnabakowski@mailo.com>
2026-04-17 21:24:21 +0200

    Add awesomewm hotkey labels, but exclude mod keys because they break the widget

--- .config/awesome/themeing/main.lua
+++ .config/awesome/themeing/main.lua
@@ -6,5 +6,28 @@ local set_wallpaper = require('themeing.set_wallpaper')
 
 beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/nabakolu/theme.lua")
 
+hotkeys_popup.widget.labels = gears.table.join(
+  hotkeys_popup.widget.labels,
+  {
+    XF86AudioMute = "󰖁",
+    XF86AudioLowerVolume = "",
+    XF86AudioRaiseVolume = "󰕾",
+    XF86AudioMicMute = "󰍭",
+    XF86MonBrightnessDown = "󰃞",
+    XF86MonBrightnessUp = "󰃠",
+    Return = "Enter",
+    BackSpace = "Backspace",
+    Delete = "Delete",
+    XF86AudioPrev = "󰒮",
+    XF86AudioNext = "󰒭",
+    XF86AudioStop = "󰓛",
+    XF86AudioPlay = "󰐊",
+    space = "Space",
+    Up = "",
+    Down = "",
+    Left = "",
+    Right = "",
+  })
+
 -- set wallpaper
 set_wallpaper()
