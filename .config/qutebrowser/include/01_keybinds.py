10d32f6852aa0e1b93b1dae48751c30369f68f33
nabakolu <lnabakowski@mailo.com>
2026-09-16 11:48:08 +0200

    Make qutebrowser have minimal dark reader in its own dir and a keybind to (de-)symlink it to the greasemonkey directory

--- .config/qutebrowser/include/01_keybinds.py
+++ .config/qutebrowser/include/01_keybinds.py
@@ -49,7 +49,8 @@ config.bind('<Ctrl-shift-m>', 'spawn --detach unique_mpv {url}')
 
 config.bind('yf', 'hint all yank')
 
-config.bind('D', 'config-cycle colors.webpage.darkmode.enabled ;; config-cycle colors.webpage.bg #FFFFFF #000000')
+config.bind('td', 'spawn --userscript toggle-dark')
+config.bind('tD', 'config-cycle colors.webpage.darkmode.enabled ;; config-cycle colors.webpage.bg #FFFFFF #000000')
 
 config.bind('gd', 'download-open')
 config.bind('gi', 'hint inputs')
